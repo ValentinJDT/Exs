f77cee59c76f67dc6d70670056965f90ea85c646
Valentin JEANDOT <valentin.jeandot@cirly.com>
2024-07-04 15:39:41 +0200

    improve human builder

--- src/main/kotlin/builder/Human.kt
+++ src/main/kotlin/builder/Human.kt
@@ -5,14 +5,16 @@ import java.awt.Color
 class Human private constructor(val name: String, val height: Int, val legs: Int, val arms: Int, val eyes: Color, override val gender: Gender): LivingEntity {
 
     data class Builder(
-        var name: String, var height: Int = 180, var legs: Int = 2, var arms: Int = 2, var eyes: Color = Color.GRAY, var gender: Gender = Gender.BOY
+        val name: String, val height: Int = 180, val legs: Int = 2, val arms: Int = 2, val eyes: Color = Color.GRAY, val gender: Gender = Gender.BOY
     ) {
-        fun name(name: String) = apply { this.name = name }
-        fun height(height: Int) = apply { this.height = height }
-        fun legs(legs: Int) = apply { this.legs = legs }
-        fun arms(arms: Int) = apply { this.arms = arms }
-        fun eyes(eyes: Color) = apply { this.eyes = eyes }
-        fun gender(gender: Gender) = apply { this.gender = gender }
+        fun name(name: String) = run { copy(name = name) }
+        fun height(height: Int) = run { copy(height = height) }
+        fun legs(legs: Int) = run { copy(legs = legs) }
+        fun arms(arms: Int) = run { copy(arms = arms) }
+        fun eyes(eyes: Color) = run { copy(eyes = eyes) }
+        fun gender(gender: Gender) = run { copy(gender = gender) }
         fun build() = Human(name, height, legs, arms, eyes, gender)
     }
+
+    override fun toString(): String = "Human(name='$name', height=$height, legs=$legs, arms=$arms, eyes=$eyes, gender=$gender)"
 }
