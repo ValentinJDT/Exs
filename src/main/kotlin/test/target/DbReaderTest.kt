4f4ca886d4fc5e6c51dbc2b3159cc0d1ad96807c
Valentin JEANDOT <valentin.jeandot@cirly.com>
2025-07-21 14:56:00 +0200

    Update DbReaderTest to use toBean method for DbReader instance

--- src/main/kotlin/test/target/DbReaderTest.kt
+++ src/main/kotlin/test/target/DbReaderTest.kt
@@ -3,11 +3,12 @@ package test.target
 import dbreader.DbReader
 import test.Test
 import test.TestClass
+import test.toBean
 
 class DbReaderTest: TestClass() {
 
     override fun beans(): Map<String, Any> = mapOf(
-        DbReader::class.java.name to DbReader(),
+        DbReader().toBean(),
     )
 
     @Test
